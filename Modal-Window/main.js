5b614ea9b15f362a9ae7f89dcacab28cff63a303
D0dikxd <shurik18608@gmail.com>
2025-10-04 16:28:55 +0300

    Add files via upload

---
+++ Modal-Window/main.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.btn-open')
+const modal = document.querySelector('.modal')
+button.addEventListener('click', () => {
+    modal.classList.add('modal--open')
+})
+modal.addEventListener('click', event => {
+    const target = event.target
+    if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
+        modal.classList.remove('modal--open')
+    }
+})
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        modal.classList.remove('modal--open')
+    }
+})
